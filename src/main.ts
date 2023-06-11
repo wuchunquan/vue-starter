36d84241237596a401be09ef67a29729c91917a1
wuchunquan <744800102@qq.com>
2023-06-12 00:09:32 +0800

    add element-plus message components css in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './assets/base.css'
-
+import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/message-box/style/css'
+import 'element-plus/es/components/notification/style/css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
